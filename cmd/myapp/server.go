f5ee01259900ec2d46e7cca5409d3990d8c66463
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-01-26 19:22:56 +0900

    :tada: init project

---
+++ cmd/myapp/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"lookncook/internal/config"
+)
+
+func init() {
+	config.LoadEnvVariables()
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "pong",
+		})
+	})
+
+	config.DatabaseInit()
+	gorm := config.DB()
+
+	dbGorm, err := gorm.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbGorm.Ping()
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
