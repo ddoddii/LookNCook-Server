ffc84e14f3aab40946e5d3ba82beb5bbd590b6e8
Soeun Uhm <uhmturks@yonsei.ac.kr>
2024-02-22 18:23:21 +0900

    ✨feat : store recipe in firestore

--- src/service/recipe.service.go
+++ src/service/recipe.service.go
@@ -2,12 +2,16 @@ package service
 
 import (
 	"cloud.google.com/go/firestore"
+	"context"
+	"encoding/json"
 	"github.com/gdsc-ys/lookncook-server/internal/utils"
+	"github.com/gdsc-ys/lookncook-server/src/constant"
+	"github.com/gdsc-ys/lookncook-server/src/model"
 	"log"
 )
 
 type RecipeService interface {
-	GenerateRecipeFromFridgeContent(imgData []byte) (string, string)
+	GenerateRecipeFromFridgeContent(imgData []byte) ([]model.Ingredient, []model.Recipe)
 }
 
 type RecipeServiceImpl struct {
@@ -15,19 +19,18 @@ type RecipeServiceImpl struct {
 	firestoreClient *firestore.Client
 }
 
-func (service *RecipeServiceImpl) GenerateRecipeFromFridgeContent(imgData []byte) (string, string) {
+func (service *RecipeServiceImpl) GenerateRecipeFromFridgeContent(imgData []byte) ([]model.Ingredient, []model.Recipe) {
 	// fridge Content
-	fridgeContent := service.generateFridgeContent(imgData)
-
+	fridgeContent, ingredients := service.generateFridgeContent(imgData)
 	// Use the fridge content as the prompt for generating the recipe
 	promptText, _ := utils.ReadFileAsString("internal/prompt/recipePrompt.txt")
 	recipePrompt := fridgeContent + promptText
-	recipeContent := service.generateRecipe(recipePrompt)
+	_, recipes := service.generateRecipe(recipePrompt)
 
-	return fridgeContent, recipeContent
+	return ingredients, recipes
 }
 
-func (service *RecipeServiceImpl) generateFridgeContent(imgData []byte) string {
+func (service *RecipeServiceImpl) generateFridgeContent(imgData []byte) (string, []model.Ingredient) {
 	promptText, err := utils.ReadFileAsString("internal/prompt/fridgeContentPrompt.txt")
 	if err != nil {
 		log.Fatal("Error occurred while reading prompt : " + err.Error())
@@ -36,15 +39,66 @@ func (service *RecipeServiceImpl) generateFridgeContent(imgData []byte) string {
 	content := service.geminiService.GenerateTextFromImage(imgData, promptText)
 	fridgeContent := utils.GeminiContentToString(content)
 
-	return fridgeContent
+	// Parse Ingredients
+	var ingredients []model.Ingredient
+	err = json.Unmarshal([]byte(fridgeContent), &ingredients)
+	if err != nil {
+		log.Fatalf("Error parsing Json in Ingredients : %v", err)
+	}
+
+	// Add Ingredients
+	service.addIngredients(ingredients)
+
+	return fridgeContent, ingredients
 }
 
-func (service *RecipeServiceImpl) generateRecipe(fridgeContent string) string {
+func (service *RecipeServiceImpl) generateRecipe(recipePrompt string) (string, []model.Recipe) {
 
-	recipe := service.geminiService.GenerateTextFromText(fridgeContent)
+	recipe := service.geminiService.GenerateTextFromText(recipePrompt)
 	recipeContent := utils.GeminiContentToString(recipe)
 
-	return recipeContent
+	// Parse Recipe
+	var recipes []model.Recipe
+	err := json.Unmarshal([]byte(recipeContent), &recipes)
+	if err != nil {
+		log.Fatalf("Error parsing JSON in Recipes : %s", err)
+	}
+
+	// Add Recipe
+	service.addRecipes(recipes)
+
+	return recipeContent, recipes
+}
+
+// Add ingredients to firestore
+func (service *RecipeServiceImpl) addIngredients(ingredients []model.Ingredient) {
+	ctx, cancel := context.WithTimeout(context.Background(), constant.FirestoreDefaultTimeout)
+	defer cancel()
+
+	for _, ingredient := range ingredients {
+		_, _, err := service.firestoreClient.
+			Collection(constant.IngredientCollection).
+			Add(ctx, ingredient)
+		if err != nil {
+			log.Printf("Failed to add ingredient: %v", err)
+		}
+	}
+
+}
+
+// Add recipes to firestore
+func (service *RecipeServiceImpl) addRecipes(recipes []model.Recipe) {
+	ctx, cancel := context.WithTimeout(context.Background(), constant.FirestoreDefaultTimeout)
+	defer cancel()
+
+	for _, recipe := range recipes {
+		_, _, err := service.firestoreClient.
+			Collection(constant.RecipeCollection).
+			Add(ctx, recipe)
+		if err != nil {
+			log.Printf("Failed to add recipe: %v", err)
+		}
+	}
 }
 
 func RecipeServiceInit(geminiService GeminiService, firestoreClient *firestore.Client) *RecipeServiceImpl {
